b57a5e0236de6325c84bc1d3981f921860fdda92
abspen1 <abspencer2097@gmail.com>
2020-08-18 13:08:02 -0700

    init

---
+++ next-project/app.js
@@ -0,0 +1,12 @@
+var Redis = require('ioredis')
+
+const dotenv = require('dotenv')
+dotenv.config()
+const redisPass = process.env.REDIS
+
+class nextProject {
+
+    constructor(input) {
+
+    }
+}
