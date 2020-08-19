c6cb7decc5a0d64ebd853bc2bbd72aa57e0fcb29
abspen1 <abspencer2097@gmail.com>
2020-08-18 23:37:04 -0700

    Getting closer with design

--- next-project/app.js
+++ next-project/app.js
@@ -1,12 +1,45 @@
 var Redis = require('ioredis')
-
 const dotenv = require('dotenv')
+const { write } = require('fs')
 dotenv.config()
 const redisPass = process.env.REDIS
 
 class nextProject {
-
-    constructor(input) {
-
+    constructor(language, project, password) {
+        this.client = new Redis({
+            port: 6379,          // Redis port
+            host: '10.10.10.1',   // Redis host
+            password: password,
+            db: 3,
+        })
+        
+        this.language = language
+        this.project = project
     }
+    async run() {
+        client.get('language').then(function (result) {
+            writeToEventLog(result);
+        });
+    }
+
+}
+
+
+function addProject() {
+    var language = $("#backend").val()
+    var project = $("#fronted").val()
+    var Np = new nextProject(language, project, redisPass)
+    Np.run()
+}
+
+function removeProject() {
+    var language = $("#backend").val()
+    var project = $("#fronted").val()
+    var padssword = $("#password").val()
+    var Rp = new nextProject(language, project, password)
+    Rp.run()
+}
+
+function writeToEventLog(text) {
+    $("#event-log").prepend(`<p class="event-fragment">${text}</p>`);
 }
