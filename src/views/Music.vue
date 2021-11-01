43eafa0a0df5182b3f6fcd11f04f48553eecf84a
abspen1 <abspencer2097@gmail.com>
2021-11-01 14:19:40 -0700

    add music page

---
+++ src/views/Music.vue
@@ -0,0 +1,33 @@
+<template>
+  <div v-if="name === 'login'">
+    <login />
+  </div>
+  <div v-else-if="name === 'user-playlists'">
+    <user-playlists />
+  </div>
+</template>
+
+<script>
+import Login from "../components/music/Login.vue";
+import UserPlaylists from "../components/music/UserPlaylists.vue";
+
+export default {
+  components: {
+    Login,
+    UserPlaylists,
+  },
+  props: ["name"],
+  data() {
+    return {};
+  },
+  methods: {},
+  computed: {
+    spotifyId() {
+      return this.$store.getters.spotifyId;
+    },
+  },
+  mounted() {},
+};
+</script>
+
+<style lang="scss" scoped></style>
