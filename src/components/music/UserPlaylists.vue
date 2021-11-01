93cad8acd2fbeb668a547bee8160107c43a83d44
abspen1 <abspencer2097@gmail.com>
2021-11-01 14:18:14 -0700

    add user playlists

---
+++ src/components/music/UserPlaylists.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-container>
+    <v-row class="mt-5" v-if="userPlaylists !== null">
+      <v-col sm="12" md="5" lg="4">
+        <user-playlists
+          :userPlaylists="userPlaylists.playlists"
+        ></user-playlists>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import UserPlaylists from "./helpers/UserPlaylists.vue";
+
+export default {
+  components: {
+    UserPlaylists,
+  },
+  data() {
+    return {
+      loadingPlaylists: false,
+      errorMessage: null,
+    };
+  },
+  methods: {
+    getPlaylists() {},
+  },
+  computed: {
+    spotifyId() {
+      return this.$store.getters.spotifyId;
+    },
+    userPlaylists() {
+      return this.$store.getters.spotifyUserPlaylists;
+    },
+  },
+  mounted() {
+    this.loadingPlaylists = true;
+    this.$store.dispatch("getUserPlaylists", { limit: 50 });
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/music/helpers/UserPlaylists.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-card class="mx-auto" max-width="500">
+    <v-list>
+      <v-list-item-group v-model="model">
+        <v-list-item v-for="item in userPlaylists" :key="item.id">
+          <v-list-item-icon>
+            <v-icon>mdi-spotify</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.name"></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "UserPlaylists",
+  props: ["userPlaylists"],
+  data() {
+    return {
+      model: 1,
+    };
+  },
+
+  mounted() {},
+
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped></style>
