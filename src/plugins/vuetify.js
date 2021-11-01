4e657e6ca8d6084716a4eb52c4534ae36b5a25a1
abspen1 <abspencer2097@gmail.com>
2021-11-01 14:18:28 -0700

    add spotify colors

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,25 +1,29 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify/lib/framework';
-import colors from 'vuetify/lib/util/colors'
+import Vue from "vue";
+import Vuetify from "vuetify/lib/framework";
+import colors from "vuetify/lib/util/colors";
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
-    theme: {
-        dark: false,
-        themes: {
-            dark: {
-                primary: "#FF7028",
-                secondary: "#FFFFFD",
-                accent: "693D2A",
-                background: "#2E2E2B",
-            },
-            light: {
-                primary: "#5D0F2F",
-                secondary: "#212121",
-                accent: colors.blueGrey.base,
-                // background: "#FFA980"
-            }
-        }
-    }
+  theme: {
+    dark: false,
+    themes: {
+      dark: {
+        primary: "#FF7028",
+        secondary: "#FFFFFD",
+        accent: "693D2A",
+        background: "#2E2E2B",
+        spotifyGreen: "#1DB954",
+        spotifyBlack: "#191414",
+      },
+      light: {
+        primary: "#5D0F2F",
+        secondary: "#212121",
+        accent: colors.blueGrey.base,
+        // background: "#FFA980"
+        spotifyGreen: "#1DB954",
+        spotifyBlack: "#191414",
+      },
+    },
+  },
 });
