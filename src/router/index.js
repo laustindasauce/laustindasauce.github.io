8cbc78a051994fc077720bf0d01284d0f42c5fb4
abspen1 <abspencer2097@gmail.com>
2021-11-01 14:18:39 -0700

    fix up and add spotify

--- src/router/index.js
+++ src/router/index.js
@@ -1,33 +1,42 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    name: "Home",
+    component: Home,
   },
   {
-    path: '/contact',
-    name: 'Contact',
+    path: "/contact",
+    name: "Contact",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Contact.vue')
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Contact.vue"),
   },
   {
-    path: '/projects/:name',
-    name: 'Project',
+    path: "/projects/:name",
+    name: "Project",
     props: true,
-    component: () => import(/* webpackChunkName: "about" */ '../views/Project.vue')
-  }
-]
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Project.vue"),
+  },
+  {
+    path: "/music/:name",
+    name: "Music",
+    props: true,
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Music.vue"),
+  },
+];
 
 const router = new VueRouter({
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
