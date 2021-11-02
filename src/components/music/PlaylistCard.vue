c7084cec0346dfcd7a0e8e4dbc23d2baf21fe558
abspen1 <abspencer2097@gmail.com>
2021-11-02 09:02:40 -0700

    add buttons and styling for links

--- src/components/music/PlaylistCard.vue
+++ src/components/music/PlaylistCard.vue
@@ -1,13 +1,25 @@
 <template>
   <v-card class="mx-auto" color="rgb(0, 0, 0, 0.1)" max-width="344">
-    <v-img :src="getImage()"></v-img>
+    <v-img
+      class="image-link"
+      @click="goToPlaylist(playlist.id)"
+      :src="getImage()"
+    ></v-img>
 
-    <v-card-title> {{ playlist.name }} </v-card-title>
+    <v-card-title>
+      <h3 class="link" @click="goToPlaylist(playlist.id)">
+        {{ playlist.name }}
+      </h3></v-card-title
+    >
 
-    <v-card-subtitle> {{ playlist.tracks.total }} tracks</v-card-subtitle>
+    <v-card-subtitle class="mt-1">
+      {{ playlist.tracks.total }} tracks</v-card-subtitle
+    >
 
     <v-card-actions>
-      <v-btn color="orange lighten-2" text> More Info </v-btn>
+      <v-btn color="orange lighten-2" @click="goToPlaylist(playlist.id)" text>
+        More Info
+      </v-btn>
     </v-card-actions>
   </v-card>
 </template>
@@ -25,8 +37,21 @@ export default {
       }
       return "default.png";
     },
+    goToPlaylist(id) {
+      this.$router.push({ name: "Single Playlist", params: { id: id } });
+    },
   },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.link:hover {
+  text-decoration: underline;
+  cursor: pointer;
+}
+.image-link:hover {
+  cursor: pointer;
+  opacity: 0.3;
+  transition: 0.3s;
+}
+</style>
