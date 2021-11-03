921bd0e11d2770e61c8e0c96b5ae90029fb89503
abspen1 <abspencer2097@gmail.com>
2021-11-02 19:12:28 -0700

    add artist item

---
+++ src/components/music/ArtistItem.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <v-row class="mb-5 mt-5" justify="center">
+      <v-img
+        lazy-src="user.png"
+        max-height="200"
+        max-width="200"
+        class="avatar"
+        @click="goToInternalLink(artist.id)"
+        :src="getImg()"
+      ></v-img>
+    </v-row>
+    <v-row class="text-center">
+      <v-col>
+        <h4
+          class="link"
+          @click="goToExternalLink(artist.external_urls.spotify)"
+        >
+          {{ artist.name }}
+        </h4>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ArtistItem",
+  props: ["artist"],
+  data() {
+    return {};
+  },
+
+  mounted() {},
+
+  methods: {
+    getImg() {
+      return this.artist.images[2].url;
+    },
+    goToExternalLink(link) {
+      window.open(link, "_blank");
+    },
+    goToInternalLink(id) {
+      this.$router.push({ name: "Single Artist", params: { id: id } });
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.avatar {
+  border-radius: 100%;
+  &:hover {
+    cursor: pointer;
+    opacity: 0.3;
+    transition: 0.3s;
+  }
+}
+.link:hover {
+  cursor: pointer;
+  text-decoration: underline;
+}
+</style>
